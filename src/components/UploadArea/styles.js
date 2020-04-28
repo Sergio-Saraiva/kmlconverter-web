8d732407afe22148b9aa7dc9677e218b153306c3
Sergio Neto <sergio.s.neto@outlook.com>
2020-04-28 12:09:38 -0300

    removed console logs

--- src/components/UploadArea/styles.js
+++ src/components/UploadArea/styles.js
@@ -66,7 +66,6 @@ export const DownloadButton = styled.button.attrs((props => ({
 })))`
     @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
     padding: 20px 100px 20px 100px;
-    ${props =>  console.log(props.disabled)}
     border: 0;
     border-radius: 4px;
     font-family: Roboto, sans-serif;
